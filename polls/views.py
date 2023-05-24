bf680a55f0d897ebbb90eed419aacd5e43c005c9
IanVonDevBarron <56098337+EmpDevReb@users.noreply.github.com>
2023-05-24 13:42:22 -0500

    Change of the 404 render Django short cut

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
 from django.http import HttpResponse
 
 from .models import Question
@@ -21,8 +21,5 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
