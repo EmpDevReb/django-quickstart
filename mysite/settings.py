a4e4ed1caaf6971d953d368782002bc30abf15a1
IanVonDevBarron <56098337+EmpDevReb@users.noreply.github.com>
2023-05-26 23:26:26 -0500

    commit on timezone

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
+# USE Nation/City format
 TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
